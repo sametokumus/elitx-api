9fb51f7ffdc50ef285b5f6f0738a774e596ee9fa
Samet <samet.okumus@codbt.com>
2024-02-05 00:18:16 +0300

    test

---
+++ app/Http/Controllers/Api/Shop/AuthController.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace App\Http\Controllers\Api\Shop;
+
+use App\Http\Controllers\Controller;
+use App\Mail\UserWelcome;
+use App\Models\ContactRule;
+use App\Models\User;
+use App\Models\UserContactRule;
+use App\Models\UserDocumentCheck;
+use App\Models\UserProfile;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+use Nette\Schema\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'user_name' => 'nullable',
+                'email' => 'required|email',
+                'phone_number' => 'required',
+                'password' => 'required'
+            ]);
+
+            $userActiveCheck = User::query()->where('email', $request->email)->where('active', 0)->count();
+
+            if ($userActiveCheck > 0) {
+                throw new \Exception('auth-004');
+            }
+
+            $userCheck = User::query()->where('email', $request->email)->count();
+
+            if ($userCheck > 0) {
+                throw new \Exception('auth-002');
+            }
+
+            $userPhoneCheck = User::query()->where('phone_number', $request->phone_number)->where('active', 1)->count();
+
+            if ($userPhoneCheck > 0) {
+                throw new \Exception('auth-003');
+            }
+
+            //Önce Kullanıcıyı oluşturuyor
+            $userId = User::query()->insertGetId([
+                'email' => $request->email,
+                'phone_number' => $request->phone_number,
+                'password' => Hash::make($request->password),
+                'token' => Str::random(60)
+            ]);
+
+            //İletişim Kurallarını oluşturuyor
+            $user_contact_rules = $request->user_contact_rules;
+            foreach ($user_contact_rules as $user_contact_rule){
+                UserContactRule::query()->insert([
+                    'user_id' => $userId,
+                    'contact_rule_id' => $user_contact_rule['contact_rule_id'],
+                    'value' => $user_contact_rule['value']
+                ]);
+            }
+
+            //Kullanıcının dökümanlarını ekliyor
+            $user_document_checks = $request->user_document_checks;
+            foreach ($user_document_checks as $user_document_check){
+                UserDocumentCheck::query()->insert([
+                    'user_id' => $userId,
+                    'document_id' => $user_document_check['document_id'],
+                    'value' => $user_document_check['value']
+                ]);
+            }
+            //Kullanıcı profilini oluşturuyor
+            $name = $request->name;
+            $surname = $request->surname;
+            UserProfile::query()->insert([
+                'user_id' => $userId,
+                'name' => $name,
+                'surname' => $surname
+            ]);
+
+            // Oluşturulan kullanıcıyı çekiyor
+            $user = User::query()->whereId($userId)->first();
+
+            //Oluşturulan Kullanıcıyı mail yolluyor
+            $user->sendApiConfirmAccount($user);
+
+            return response(['message' => 'Kullanıcı başarıyla oluşturuldu sisteme giriş için epostanızı kontrol ediniz.','status' => 'success']);
+        } catch (ValidationException $validationException) {
+            return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
+        } catch (QueryException $queryException) {
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001','error' => $queryException->getMessage()]);
+        } catch (\Exception $exception){
+            if ($exception->getMessage() == 'auth-002'){
+                return  response(['message' => 'Girdiğiniz eposta adresi kullanılmaktadır.','status' => 'auth-002']);
+            }
+            if ($exception->getMessage() == 'auth-003'){
+                return  response(['message' => 'Girdiğiniz telefon numarası kullanılmaktadır.','status' => 'auth-003']);
+            }
+            if ($exception->getMessage() == 'auth-004'){
+                return  response(['message' => 'Bu e-posta adresi ile bir hesap bulunmaktadır. Yeniden giriş yaparak hesabınızı aktifleştirebilirsiniz.','status' => 'auth-003']);
+            }
+            return  response(['message' => 'Hatalı işlem.','status' => 'error-001', 'err' => $exception->getMessage()]);
+        }
+
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            $user = User::query()->where('email', $request->email)->first();
+
+            if ($user->active == 0){
+                User::query()->where('email', $request->email)->update([
+                    'active' => 1
+                ]);
+            }
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                throw new \Exception('auth-001');
+            }
+
+            $userToken = $user->createToken('api-token', ['role:user'])->plainTextToken;
+            User::query()->where('id', $user->id)->update([
+                'token' => $userToken
+            ]);
+
+            $user->token = $userToken;
+
+            return  response(['message' => 'Başarılı.','status' => 'success', 'object' => ['user'=>$user]]);
+        } catch (ValidationException $validationException) {
+            return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
+        } catch (QueryException $queryException) {
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
+        } catch (\Exception $exception){
+            if ($exception->getMessage() == 'auth-001'){
+                return  response(['message' => 'Eposta veya şifre hatalı.','status' => 'auth-001']);
+            }
+            return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        }
+    }
+
+    public function logout()
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            return response(['message' => 'Çıkış başarılı.','status' => 'success']);
+        } catch (\Exception $exception){
+            return response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+
+    protected function verify(Request $request)
+    {
+        try {
+            $user = User::query()->where('token', $request['token'])->first();
+            if ($user->verified == 1 && $user->email_verified_at != null) {
+                throw new \Exception('validation-002');
+            }
+            $user->email_verified_at = now();
+            $user->verified = true;
+            $user->active = true;
+            $user->token = null;
+            $user->save();
+            /*
+               $setDelay = Carbon::parse($user->email_verified_at)->addSeconds(10);
+               Bu kısımda isterseniz Kullanıcıya Hoşgeldinizi Maili İçin Gecikme Verebilirsiniz.
+               Mail::queue(new \App\Mail\UserWelcome($user->name, $user->email))->delay($setDelay);
+              */
+            Mail::queue(new UserWelcome($user->name, $user->email));
+            return response(['message' => 'Kullanıcı epostası doğrulandı.','status' => 'success']);
+        } catch (ValidationException $validationException) {
+            return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
+        } catch (QueryException $queryException) {
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
+        } catch (\Exception $exception){
+            if ($exception->getMessage() == 'validation-002'){
+                return response('Eposta adresi daha önceden doğrulanmış.');
+            }
+            return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        }
+
+
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+
+    protected function resend(Request $request)
+    {
+
+        try {
+            $user = User::query()->where('email', $request['email'])->first();
+            if ($user->hasVerifiedEmail()) {
+                throw new \Exception('validation-002');
+            }
+            $user->sendApiConfirmAccount($user);
+            return response(['message' => 'Yeniden eposta gönderildi.', 'status' => 'success']);
+        } catch (ValidationException $validationException) {
+            return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
+        } catch (QueryException $queryException) {
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
+        } catch (\Exception $exception){
+            if ($exception->getMessage() == 'validation-002'){
+                return response('Eposta adresi daha önceden doğrulanmış.');
+            }
+            return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        }
+    }
+}
---
+++ app/Http/Controllers/Api/Shop/ResetPasswordController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers\Api\Shop;
+
+use App\Http\Controllers\Controller;
+use App\Models\ResetPassword;
+use App\Models\User;
+use App\Notifications\ResetPasswordNotify;
+use App\Notifications\ResetPasswordSuccess;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Nette\Schema\ValidationException;
+
+class ResetPasswordController extends Controller
+{
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|string|email|exists:users'
+            ]);
+
+            $user = User::query()->where('email', $request['email'])->first();
+            if (!$user) {
+                throw new \Exception('validation-003');
+            }
+            $resetpassword = ResetPassword::query()->updateOrCreate(
+                [
+                    'email' => $user->email,
+                ],
+                [
+                    'email' => $user->email,
+                    'token' => Str::random(45),
+                ]
+            );
+            if ($user && $resetpassword) {
+                $user->notify(new ResetPasswordNotify($resetpassword->token));
+            }
+            return response()->json(['message' => 'E-Posta adresinize şifre sıfırlama bağlantısı gönderildi.', 'status' => 'success']);
+        } catch (ValidationException $validationException) {
+            return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
+        } catch (QueryException $queryException) {
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
+        } catch (\Exception $exception){
+            if ($exception->getMessage() == 'validation-003'){
+                return response('E-Posta adresi bulunamadı.');
+            }
+            return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        }
+    }
+
+    /**
+     * @param $token
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     */
+    public function find($token) {
+        try {
+            $resetpassword = ResetPassword::query()->where('token', $token)->first();
+            if (!$token) {
+                throw new \Exception('validation-004');
+            }
+            if (Carbon::parse($resetpassword->created_at)->addMinutes(720)->isPast()) {
+                $resetpassword->delete();
+                throw new \Exception('validation-004');
+            }
+            return response()->json(['message' => 'Başarılı.', 'status' => 'success']);
+        } catch (ValidationException $validationException) {
+            return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
+        } catch (QueryException $queryException) {
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
+        } catch (\Exception $exception){
+            if ($exception->getMessage() == 'validation-004'){
+                return response('Token geçersiz.');
+            }
+            return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     */
+    public function resetPassword(Request $request) {
+//        $validator = $request->validate([
+//            'email' => 'required|string|email|exists:users',
+//            'token' => 'required|string',
+//            'password' => 'required|string|min:6|confirmed',
+//        ]);
+//        if ($validator->fails()) {
+//            $message['errors'] = $validator->errors();
+//            return response()->json(['message' => $message, 'code' => 400]);
+//        }
+        try {
+            $resetpassword = ResetPassword::query()->updateOrCreate(
+                [
+                    'email' => $request->email,
+                    'token' => $request->token,
+                ]
+            )->first();
+            if (!$resetpassword) {
+                throw new \Exception('validation-003');
+            }
+            $user = User::query()->where('email', $request->email)->first();
+            if (!$user) {
+                throw new \Exception('validation-005');
+            }
+            User::query()->where('email', $request->email)->update([
+                'password' => Hash::make($request->password)
+            ]);
+            ResetPassword::query()->where('email', $request->email)->delete();
+            $user->notify(new ResetPasswordSuccess());
+            return response()->json(['message' => 'Kullanıcı şifresi başarıyla değiştirildi.', 'status' => 'success']);
+        } catch (ValidationException $validationException) {
+            return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
+        } catch (QueryException $queryException) {
+            return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
+        } catch (\Exception $exception){
+            if ($exception->getMessage() == 'validation-003'){
+                return response('E-Posta adresi bulunamadı.');
+            }
+            if ($exception->getMessage() == 'validation-005'){
+                return response('Kullanıcı bulunamadı.');
+            }
+            return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        }
+    }
+}
