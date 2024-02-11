18a5d0090039459be0de93ae07f041a711ef2ca8
Samet <samet.okumus@codbt.com>
2024-02-12 00:14:43 +0300

    test

--- app/Http/Controllers/Api/Shop/ResetPasswordController.php
+++ app/Http/Controllers/Api/Shop/ResetPasswordController.php
@@ -90,45 +90,31 @@ class ResetPasswordController extends Controller
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
      */
     public function newPassword(Request $request) {
-//        $validator = $request->validate([
-//            'email' => 'required|string|email|exists:users',
-//            'token' => 'required|string',
-//            'password' => 'required|string|min:6|confirmed',
-//        ]);
-//        if ($validator->fails()) {
-//            $message['errors'] = $validator->errors();
-//            return response()->json(['message' => $message, 'code' => 400]);
-//        }
         try {
-            $resetpassword = ResetPassword::query()->updateOrCreate(
-                [
-                    'email' => $request->email,
-                    'token' => $request->token,
-                ]
-            )->first();
+            $resetpassword = ResetPassword::query()->where('token', $request->token)->first();
             if (!$resetpassword) {
                 throw new \Exception('validation-003');
             }
-            $user = User::query()->where('email', $request->email)->first();
+            $user = Shop::query()->where('email', $resetpassword->email)->where('active', 1)->first();
             if (!$user) {
                 throw new \Exception('validation-005');
             }
-            User::query()->where('email', $request->email)->update([
+            Shop::query()->where('email', $resetpassword->email)->update([
                 'password' => Hash::make($request->password)
             ]);
-            ResetPassword::query()->where('email', $request->email)->delete();
-            $user->notify(new ResetPasswordSuccess());
-            return response()->json(['message' => 'Kullanıcı şifresi başarıyla değiştirildi.', 'status' => 'success']);
+            ResetPassword::query()->where('email', $resetpassword->email)->delete();
+//            $user->notify(new ResetPasswordSuccess());
+            return response()->json(['message' => 'Mağaza şifresi başarıyla değiştirildi.', 'status' => 'success']);
         } catch (ValidationException $validationException) {
             return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
         } catch (QueryException $queryException) {
             return  response(['message' => 'Hatalı sorgu.','status' => 'query-001']);
         } catch (\Exception $exception){
             if ($exception->getMessage() == 'validation-003'){
-                return response('E-Posta adresi bulunamadı.');
+                return  response(['message' => 'Şifre yenileme talebi bulunamadı!','status' => 'validation-003']);
             }
             if ($exception->getMessage() == 'validation-005'){
-                return response('Kullanıcı bulunamadı.');
+                return  response(['message' => 'Kullanıcı bulunamadı!','status' => 'validation-003']);
             }
             return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
         }
