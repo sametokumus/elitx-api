e684f78a05e1bbf3de404c34685a992e2d5a7ac8
Samet <samet.okumus@codbt.com>
2024-03-02 18:37:21 +0300

    test

--- app/Http/Controllers/Api/Shop/ProductController.php
+++ app/Http/Controllers/Api/Shop/ProductController.php
@@ -62,15 +62,15 @@ class ProductController extends Controller
                 ]);
             }
 
-            $variations = json_decode($request->variations);
-            foreach ($variations as $variation){
-                ProductVariation::query()->insert([
-                    'variation_group_id' => $variation->group_id,
-                    'name' => $variation->name,
-                    'stock_quantity' => $variation->stock_quantity,
-                    'price' => $variation->price
-                ]);
-            }
+//            $variations = json_decode($request->variations);
+//            foreach ($variations as $variation){
+//                ProductVariation::query()->insert([
+//                    'variation_group_id' => $variation->group_id,
+//                    'name' => $variation->name,
+//                    'stock_quantity' => $variation->stock_quantity,
+//                    'price' => $variation->price
+//                ]);
+//            }
 
             $categories = json_decode($request->categories);
             foreach ($categories as $category){
