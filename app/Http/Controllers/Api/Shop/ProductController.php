3b4324f7f5c3ff18560a6cec3e3a08b3acf05117
Samet <samet.okumus@codbt.com>
2024-03-02 23:18:15 +0300

    test

--- app/Http/Controllers/Api/Shop/ProductController.php
+++ app/Http/Controllers/Api/Shop/ProductController.php
@@ -65,10 +65,10 @@ class ProductController extends Controller
             $variations = json_decode($request->variations);
             foreach ($variations as $variation){
                 ProductVariation::query()->insert([
-                    'variation_group_id' => $variation->variationGroup,
-                    'name' => $variation->variationName,
-                    'stock_quantity' => $variation->variationPrice,
-                    'price' => $variation->variationQuantity
+                    'variation_group_id' => $variation->group_id,
+                    'name' => $variation->name,
+                    'stock_quantity' => $variation->stock_quantity,
+                    'price' => $variation->price
                 ]);
             }
 
