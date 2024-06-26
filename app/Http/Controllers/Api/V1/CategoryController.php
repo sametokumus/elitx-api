84be356a8d5a0db470f1822c77e8f62f7a9bd8f2
Samet <samet.okumus@codbt.com>
2024-06-26 19:59:40 +0300

    test

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -38,7 +38,7 @@ class CategoryController extends Controller
     public function getCategoriesByParentId($id)
     {
         try {
-            $categories = Category::query()->where('active', 1)->where('parent_id', $id)->toSql();
+            $categories = Category::query()->where('active', 1)->where('parent_id', $id)->get();
 
 //            $categoryTree = $this->buildTree($categories);
 
