f98492cf709ee490e16564b54463e315018502c9
Samet <samet.okumus@codbt.com>
2024-07-10 17:46:50 +0300

    test

--- app/Http/Controllers/Api/Admin/OrderController.php
+++ app/Http/Controllers/Api/Admin/OrderController.php
@@ -75,7 +75,7 @@ class OrderController extends Controller
                 ->leftJoin('order_statuses', 'order_statuses.id', '=', 'orders.status_id')
                 ->where('order_statuses.run_on', 1)
                 ->where('orders.active', 1)
-                ->get(['orders.id', 'orders.order_id', 'orders.created_at as order_date', 'orders.updated_at as order_update_date', 'orders.total', 'orders.status_id',
+                ->get(['orders.id', 'orders.order_id', 'orders.created_at as order_date', 'orders.updated_at as order_update_date', 'orders.total', 'orders.currency', 'orders.status_id',
                     'orders.user_id', 'orders.is_paid'
                 ]);
             foreach ($orders as $order) {
@@ -92,6 +92,7 @@ class OrderController extends Controller
                     $payment = Payment::query()->where('order_id', $order->order_id)->where('active', 1)->where('is_paid', 1)->first();
                     $order['payment'] = $payment;
                 }
+                $order['user'] = User::query()->where('id', $order->user_id)->first();
 
             }
             return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['orders' => $orders]]);
