fcae6380ea1dbd5ed0c5ae2c8e4dc0b6f826ef1f
Samet <samet.okumus@codbt.com>
2024-10-12 00:19:08 +0300

    test

--- app/Http/Controllers/Api/V1/CitiesController.php
+++ app/Http/Controllers/Api/V1/CitiesController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\City;
 use App\Models\District;
 use App\Models\Districts2;
+use App\Models\ilceler;
+use App\Models\iller;
+use App\Models\mahalleler;
 use App\Models\Neighbourhood;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
@@ -15,11 +18,39 @@ class CitiesController extends Controller
     public function getUpdateNeighbours()
     {
         try {
-            $cities = City::query()->where('country_id', 223)->get();
-            foreach ($cities as $city){
-                District::query()->where('city_id', $city->id)->delete();
+            $iller = iller::query()->get();
+            foreach ($iller as $il){
+                $il_id = City::query()->insertGetId([
+                    'name' => $il->il_adi,
+                    'country_id' => 223
+                ]);
+
+                $ilceler = ilceler::query()->where('il_id', $il_id)->get();
+                foreach ($ilceler as $ilce){
+                    $ilce_id = District::query()->insertGetId([
+                        'name' => $ilce->ilce_adi,
+                        'city_id' => $il_id
+                    ]);
+
+                    $mahalleler = mahalleler::query()->where('il_id', $il_id)->where('ilce_id', $ilce_id)->get();
+                    foreach ($mahalleler as $mahalle){
+                        Neighbourhood::query()->insertGetId([
+                            'name' => $mahalle->mahalle_adi,
+                            'postal_code' => $mahalle->posta_kodu,
+                            'district_id' => $ilce_id
+                        ]);
+
+                    }
+
+                }
             }
-            City::query()->where('country_id', 223)->delete();
+
+//
+//            $cities = City::query()->where('country_id', 223)->get();
+//            foreach ($cities as $city){
+//                District::query()->where('city_id', $city->id)->delete();
+//            }
+//            City::query()->where('country_id', 223)->delete();
 
 
 //            $old_dists = Districts2::query()->get();
