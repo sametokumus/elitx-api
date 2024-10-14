aeda60d9ee264f45f24746108b0b83f3b449596b
Samet <samet.okumus@codbt.com>
2024-10-14 12:37:59 +0300

    test

--- app/Http/Controllers/Api/V1/AdvertController.php
+++ app/Http/Controllers/Api/V1/AdvertController.php
@@ -503,10 +503,8 @@ class AdvertController extends Controller
             return response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.', 'status' => 'validation-001']);
         } catch (QueryException $queryException) {
             return response(['message' => 'Hatalı sorgu.', 'status' => 'query-001', 'a' => $queryException->getMessage()]);
-//        } catch (\Throwable $throwable) {
-//            return response(['message' => 'Hatalı işlem.', 'status' => 'error-001', 'er' => $throwable->getMessage(), 'ln' => $throwable->getLine()]);
-        } catch (\Exception $exception){
-            return  response(['message' => 'Hatalı işlem.','status' => 'error-001']);
+        } catch (\Throwable $throwable) {
+            return response(['message' => 'Hatalı işlem.', 'status' => 'error-001', 'er' => $throwable->getMessage(), 'ln' => $throwable->getLine()]);
         }
 
     }
